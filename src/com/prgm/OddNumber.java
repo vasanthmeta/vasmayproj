144a6462ff3d0cb85258087af8a737106200015c
vasanthmeta <itmetavas@gmail.com>
2022-05-16 11:23:00 +0530

    task done

---
+++ src/com/prgm/OddNumber.java
@@ -0,0 +1,13 @@
+package com.prgm;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		int last=50;
+		for (int i = 0; i <=50; i++) {
+			if (i%2!=0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
